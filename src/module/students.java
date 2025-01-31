397dde2cd2a9a761b4d05e6d6db33d37f11cfc03
ANISETTY AKASH <akashanisetty7@gmail.com>
2025-01-31 11:24:52 +0530

    added student class

---
+++ src/module/students.java
@@ -0,0 +1,44 @@
+package src.module;
+class students 
+{
+  private String id;
+  private String name;
+  private int age;
+  private float grade;   
+
+
+  public students(String id,String name,int age,float grade)
+  {
+    this.age=age;
+    this.grade=grade;
+    this.name = name;
+    this.id = id;
+
+  }
+  public String getName(){
+    return name;
+  }
+  public void setName(String name){
+    this.name=name;
+  }
+  public String getId(){
+    return id;
+  }
+  public int getAge(){
+    return age;
+  }
+  public void setAge(int age){
+    this.age=age;
+  }
+  public float getGrade(){
+    return grade;
+  }
+  public void setGrade(float grade){
+    this.grade=grade;
+  }
+  public String toString()
+  {
+    return "Stuent ID: "+id+", Name: "+name+", Age: "+age+", Grade: "+grade;
+  }
+  
+}
